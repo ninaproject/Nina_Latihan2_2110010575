597a4fa6d895dea83db081adadbfef8aa2e48782
ninaproject <ninabrays12@gmail.com>
2023-08-03 15:58:00 -0900

    Membuat class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/MahasiswaBeraksi.java
@@ -0,0 +1,13 @@
+
+package universitas;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa Nina = new Mahasiswa ("2110010575","Nina","TI");
+        System.out.println("Nama Mahasiswa = "+ Nina.getNama());
+        System.out.println("Npm Mahasiswa = "+ Nina.getNpm());
+        System.out.println("Prodi Mahasiswa = "+ Nina.getProdi());
+
+
+    }
+}
